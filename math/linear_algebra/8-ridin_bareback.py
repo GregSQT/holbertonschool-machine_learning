7df84faa56926392bc6b6f1ad4881980b2ff1e2c
GregSQT <9604@holbertonstudents.com>
2024-07-29 14:15:52 +0000

    message

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Function used to performs matrix multiplication
+"""
+
+
+def mat_mul(mat1, mat2):
+    """
+    Input : 2 matrix
+    Return : a matrix issue frome the multiplication of the 2 matrix in parameter
+    """
+    if len(mat1[0]) == len(mat2):
+        result_matrix = []
+        for row in range(len(mat1)):
+            new_row = []
+            for col2 in range(len(mat2[0])):
+                number = 0
+                for col1 in range(len(mat1[0])):
+                    number += (mat1[row][col1] * mat2[col1][col2])
+                new_row.append(number)
+            result_matrix.append(new_row)
+        return result_matrix
+    else:
+        return None
