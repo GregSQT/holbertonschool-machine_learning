aa87fb53a64b1ae9896bc22c44bb8ad50ccc4380
GregSQT <9604@holbertonstudents.com>
2024-07-29 13:06:21 +0000

    message

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Function used to concatenates two arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Input :  2 arrays
+    Return : The concatenation of the arrays
+    """
+    return [arr1 + arr2]
