908cf0a3255994a8a94a80dbef6dac112aef641e
GregSQT <9604@holbertonstudents.com>
2024-07-29 12:41:33 +0000

    message

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Function used to add arrays
+"""
+
+
+def add_arrays(arr1, arr2):
+    """
+    Input :  2 arrays
+    Return : The sum of the arrays
+    """
+    if len(arr1) != len(arr2):
+        return None
+    return [(arr1[i] + arr2[i]) for i in range(len(arr1))]
