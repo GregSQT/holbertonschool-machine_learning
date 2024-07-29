ccd7a1d0ce025994c526484893b31bfccfaabfbe
GregSQT <9604@holbertonstudents.com>
2024-07-29 12:51:56 +0000

    message

---
+++ math/linear_algebra/5-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+add_matrices2D = __import__('5-across_the_planes').add_matrices2D
+
+mat1 = [[1, 2], [3, 4]]
+mat2 = [[5, 6], [7, 8]]
+print(add_matrices2D(mat1, mat2))
+print(mat1)
+print(mat2)
+print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
+alexa@ubuntu-xenial:linear_algebra$ ./5-main.py 
+[[6, 8], [10, 12]]
+[[1, 2], [3, 4]]
+[[5, 6], [7, 8]]
+None
