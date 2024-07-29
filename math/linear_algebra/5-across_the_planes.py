57a190b5a6d8fdd9ff20372607c778ad32b9e0ab
GregSQT <9604@holbertonstudents.com>
2024-07-29 13:02:46 +0000

    message

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 """
-Function used to sum 2 arrays element wise
+Function used to sum 2 matrix
 """
 
 
-def add_arrays(arr1, arr2):
+def add_matrices2D(mat1, mat2):
     """
-    Input : 2 arrays
-    Return : The sum of the arrays
+    Input : 2 matrix
+    Return : The sum of the matrix as a list
     """
-    if len(arr1) != len(arr2):
+    if len(mat1) != len(mat2):
         return None
-    return [(arr1[i] + arr2[i]) for i in range(len(arr1))]
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+    return [[(mat1[i][j] + mat2[i][j]) for j in range(len(mat1[0]))]
+            for i in range(len(mat1))]
--- math/linear_algebra/5-main.py
+++ math/linear_algebra/5-main.py
@@ -8,8 +8,3 @@ print(add_matrices2D(mat1, mat2))
 print(mat1)
 print(mat2)
 print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
-alexa@ubuntu-xenial:linear_algebra$ ./5-main.py 
-[[6, 8], [10, 12]]
-[[1, 2], [3, 4]]
-[[5, 6], [7, 8]]
-None
