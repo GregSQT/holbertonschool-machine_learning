81e1a2fb9cca53933454070b1e7e6bdbe9491652
GregSQT <9604@holbertonstudents.com>
2024-07-30 09:01:11 +0000

    message

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function used to multiply 2 matrix
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Input : the 2 matrix
+    Return : the result of the multiplication
+    """
+    return np.matmul(mat1, mat2)
