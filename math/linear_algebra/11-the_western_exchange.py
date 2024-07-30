1cf75dfc71363e470caf717c043e0b0c5aede136
GregSQT <9604@holbertonstudents.com>
2024-07-30 08:16:15 +0000

    message

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Function used to transposes matrix
+"""
+
+
+def np_transpose(matrix):
+    """
+    Input : a matrix
+    Returns the matrix transposed
+    """
+    return matrix.transpose()
