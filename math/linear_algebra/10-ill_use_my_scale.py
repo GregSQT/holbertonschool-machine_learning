74fb1f660eadc070d3317ccbe28ab2065293de6a
GregSQT <9604@holbertonstudents.com>
2024-07-30 08:10:02 +0000

    message

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function used to calculate the shape of a matrix
+using numpy
+"""
+
+
+def np_shape(matrix):
+    """
+    Input : a patrix
+    Returns the shape of the matrix
+    """
+    return matrix.shape
