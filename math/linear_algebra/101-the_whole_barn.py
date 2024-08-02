1aa306c5e450fbe2272fd0543f15f64d73f4c233
GregSQT <9604@holbertonstudents.com>
2024-08-02 07:42:02 +0000

    message

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -12,7 +12,7 @@ def matrix_shape(matrix):
     if type(matrix[0]) != list:
         return [len(matrix)]
     else:
-        return [len(matrix)] + shape(matrix[0])
+        return [len(matrix)] + matrix_shape(matrix[0])
 
 
 def elements_addition(mat1, mat2):
@@ -24,7 +24,7 @@ def elements_addition(mat1, mat2):
     if (type(mat1) and type(mat2)) == list:
         for i in range(len(mat1)):
             if type(mat1[i]) == list:
-                new_matrix.append(rec_matrix(mat1[i], mat2[i]))
+                new_matrix.append(elements_addition(mat1[i], mat2[i]))
             else:
                 new_matrix.append(mat1[i] + mat2[i])
         return new_matrix
@@ -38,5 +38,5 @@ def add_matrices(mat1, mat2):
     if matrix_shape(mat1) != matrix_shape(mat2):
         return None
     else:
-        new_matrix = new_matrix(mat1, mat2)
+        new_matrix = elements_addition(mat1, mat2)
         return new_matrix
