ad4e9442fdd5882b219833a751e27fc9b86ef70e
GregSQT <9604@holbertonstudents.com>
2024-08-02 07:35:39 +0000

    message

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -3,10 +3,40 @@
 Function used to adds two matrices
 """
 
-def np_shape(matrix):
+
+def matrix_shape(matrix):
     """
     Input : a matrix
     Returns the shape of the matrix
     """
-    return matrix.shape
+    if type(matrix[0]) != list:
+        return [len(matrix)]
+    else:
+        return [len(matrix)] + shape(matrix[0])
 
+
+def elements_addition(mat1, mat2):
+    """
+    Input : 2 matrix
+    Output : the recursive addition of the matrix element-wise
+    """
+    new_matrix = []
+    if (type(mat1) and type(mat2)) == list:
+        for i in range(len(mat1)):
+            if type(mat1[i]) == list:
+                new_matrix.append(rec_matrix(mat1[i], mat2[i]))
+            else:
+                new_matrix.append(mat1[i] + mat2[i])
+        return new_matrix
+
+
+def add_matrices(mat1, mat2):
+    """
+    Input : 2 matrix
+    Output : the addition of the matrix element-wise
+    """
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+    else:
+        result_matrix = new_matrix(mat1, mat2)
+        return result_matrix
