230873b02beb22529776cafd9ffe8c04fb120415
GregSQT <9604@holbertonstudents.com>
2024-08-07 14:56:15 +0000

    message

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+"""
+Function used to draw a line
+"""
+
 
 def line():
+    """
+    No argument
+    """
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
