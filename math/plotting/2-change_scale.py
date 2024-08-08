b89a4648a002978b62af15a32f769a476448e21c
GregSQT <9604@holbertonstudents.com>
2024-08-08 13:44:47 +0000

    message

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-import numpy as np
-import matplotlib.pyplot as plt
 """
 Function used to plot x and y as a line graph
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+"""
+Function without input
+"""
 
 def change_scale():
-    """
-    Function without input
-    """
     x = np.arange(0, 28651, 5730)
     r = np.log(0.5)
     t = 5730
