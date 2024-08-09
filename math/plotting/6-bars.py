e5d7493d99be38760e2de84cab3aa78faf97d313
GregSQT <9604@holbertonstudents.com>
2024-08-09 12:56:14 +0000

    message

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -23,10 +23,10 @@ def bars():
     colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
 
     for i in range(len(fruit)):
-        plt.bar(names, fruit[i], 
-        bottom=np.sum(fruit[:i], axis=0), 
-        color=colors[i], 
-        label=fruit_name[i], 
+        plt.bar(names, fruit[i],
+        bottom=np.sum(fruit[:i], axis=0),
+        color=colors[i],
+        label=fruit_name[i],
         width=0.5)
 
     plt.title("Number of Fruit per Person")
