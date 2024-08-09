dc14f64988a123fdf47919eec9db10c7c16bce07
GregSQT <9604@holbertonstudents.com>
2024-08-09 13:01:40 +0000

    message

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -18,19 +18,23 @@ def bars():
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
-    names = ['Farrah', 'Fred', 'Felicia']
-    fruit_name = ['apples', 'bananas', 'oranges', 'peaches']
-    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+    rows = ('apples', 'bananas', 'oranges', 'peaches')
+    columns = ('Farrah', 'Fred', 'Felicia')
+    index = columns
+    colors = ('red', 'yellow', '#ff8000', '#ffe5b4')
+    n_rows = len(fruit)
+    bar_width = 0.5
+    y_offset = np.zeros(len(columns))
 
-    for i in range(len(fruit)):
-        plt.bar(names, fruit[i],
-            bottom=np.sum(fruit[:i], axis=0),
-            color=colors[i],
-            label=fruit_name[i],
-            width=0.5)
 
-    plt.title("Number of Fruit per Person")
-    plt.ylabel("Quantity of Fruit")
-    plt.yticks(np.arange(0, 90, 10))
+    for row in range(n_rows):
+        plt.bar(index, fruit[row], bar_width, bottom=y_offset,
+            color=colors[row], label=rows[row])
+        y_offset = y_offset + fruit[row]
+
+
     plt.legend()
+    plt.yticks(np.arange(0, 90, 10))
+    plt.ylabel('Quantity of Fruit')
+    plt.title("Number of Fruit per Person")
     plt.show()
