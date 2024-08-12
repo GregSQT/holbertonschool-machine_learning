16804686f7b6f1abe1a2c084a3ecd6671041a705
GregSQT <9604@holbertonstudents.com>
2024-08-12 18:03:28 +0200

    message

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" 
+Function that calculates the derivative of a polynomial
+"""
+
+
+def poly_derivative(poly):
+    """ find derivative"""
+    if type(poly) is not list or len(poly) == 0:
+        return None
+    if len(poly) == 1:
+        return [0]
+    for x in range(1, len(poly)):
+        poly[x] = poly[x] * x
+    poly.pop(0)
+    return poly
