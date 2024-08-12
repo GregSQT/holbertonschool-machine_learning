f09e7cc2ac3182056311c7c6fbd14f4e929844a4
GregSQT <9604@holbertonstudents.com>
2024-08-12 17:47:07 +0200

    message

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+A function that calculates a cuadratic sum
+"""
+
+
+def summation_i_squared(n):
+    """
+    Input : n (limit)
+    Output : sum of i^2 with i from 1 to n
+    """
+    if n < 1 or type(n) != int:
+        return None
+    else:
+        iterations = range(1, n + 1)
+    return sum(map(lambda n: pow(n, 2), iterations))
