9bff8f65493178204c60c04d1bcc6e753a939254
GregSQT <9604@holbertonstudents.com>
2024-08-14 16:45:19 +0200

    message

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -17,7 +17,9 @@ class Node:
         self.depth = depth
 
     def max_depth_below(self):
-        """ Computes the depth of a decision tree using recursion. """
+        """
+        Recursively calculate the depth of a decision tree
+        """
         def max_depth_recursive(node, depth):
             if node.is_leaf is True:
                 return depth
