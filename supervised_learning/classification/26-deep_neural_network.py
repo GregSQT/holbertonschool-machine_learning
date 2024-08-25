fe67f9493cc02ecdc59f755eb5296b4fe710ac72
GregSQT <9604@holbertonstudents.com>
2024-08-25 14:06:42 +0200

    message

--- supervised_learning/classification/26-deep_neural_network.py
+++ supervised_learning/classification/26-deep_neural_network.py
@@ -72,19 +72,19 @@ class DeepNeuralNetwork:
         A = cache["A" + str(L)]
         dZ = A - Y
 
-        for l in range(L, 0, -1):
-            A_prev = cache["A" + str(l - 1)]
-            W = self.__weights["W" + str(l)]
-            b = self.__weights["b" + str(l)]
+        for li in range(L, 0, -1):
+            A_prev = cache["A" + str(li - 1)]
+            W = self.__weights["W" + str(li)]
+            b = self.__weights["b" + str(li)]
 
             dW = (1 / m) * np.matmul(dZ, A_prev.T)
             db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
             dA = np.matmul(W.T, dZ)
 
-            self.__weights["W" + str(l)] -= alpha * dW
-            self.__weights["b" + str(l)] -= alpha * db
+            self.__weights["W" + str(li)] -= alpha * dW
+            self.__weights["b" + str(li)] -= alpha * db
 
-            if l > 1:
+            if li > 1:
                 dZ = dA * (A_prev * (1 - A_prev))
 
     def train(self, X, Y, iterations=5000, alpha=0.05,
