a231d58d756d654391b42624a9b37c2c8ec76028
GregSQT <gregory.souquet@hotmail.fr>
2024-09-07 19:57:29 +0200

    plotting

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -21,7 +21,7 @@ def bars():
     n_rows = len(fruit)
     bar_width = 0.5
     y_offset = np.zeros(len(columns))
-    
+
     for row in range(n_rows):
         plt.bar(index, fruit[row], bar_width, bottom=y_offset,
                 color=colors[row], label=rows[row])
