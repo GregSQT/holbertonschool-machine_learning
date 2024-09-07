98d537a6ef1078d26ee1404fa339817fa29bd03d
GregSQT <gregory.souquet@hotmail.fr>
2024-09-07 19:49:37 +0200

    keras

--- supervised_learning/keras/9-model.py
+++ supervised_learning/keras/9-model.py
@@ -6,20 +6,20 @@ import tensorflow.keras as K
 
 
 def save_model(network, filename):
-    """saves an entire model"""
-    network.save(
-        filepath=filename,
-        overwrite=True,
-        include_optimizer=True
-    )
+    """
+    saves an entire model
+    param network:  the model to save
+    param filename: is the path of the file that the model should be saved to
+    return: None
+    """
+    K.models.save_model(model=network, filepath=filename)
     return None
 
 
 def load_model(filename):
-    """loads an entire model"""
-    network = K.models.load_model(
-        filepath=filename,
-        custom_objects=None,
-        compile=True
-    )
-    return network
+    """
+    loads an entire model
+    param filename: the path of the file that the model should be loaded from
+    return: the loaded model
+    """
+    return K.models.load_model(filepath=filename)
