3d4bab7bbc6af674c1cc044f16553dbe09ae6efc
GregSQT <gregory.souquet@hotmail.fr>
2024-09-07 19:52:13 +0200

    plotting

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import numpy as np
-import matplotlib.pyplot as plt
 """
 Function used to draw a line
 """
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def line():
@@ -12,10 +12,8 @@ def line():
     Will draw a line from 0 to 10 on axis
     the line will be a representation of cube of x
     """
-
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-
     plt.xlim(0, 10)
     plt.plot(y, color='r')
     plt.show()
