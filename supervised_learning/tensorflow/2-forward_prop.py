3386690dad80df6909e52ff4715ef8ae6c1ea6c2
GregSQT <gregory.souquet@hotmail.fr>
2024-09-07 05:16:39 +0200

    tensor

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -2,10 +2,10 @@
 """
 Exercice 2 - Forward Propagation
 """
-import tensorflow as tf
+
+import tensorflow.compat.v1 as tf
 # Import the create_layer function from the 1-create_layer.py file
 create_layer = __import__('1-create_layer').create_layer
-
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """function that creates the forward propagation graph for the nn"""
