8736edffda90b2529ee5710816fab5ff4988058d
GregSQT <gregory.souquet@hotmail.fr>
2024-09-07 20:04:14 +0200

    plotting

---
+++ math/plotting/100-gradient.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Exercice 7 : Function used to create a scatter plot 
+             of sampled elevations on a mountain
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+np.random.seed(5)
+
+x = np.random.randn(2000) * 10
+y = np.random.randn(2000) * 10
+z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+
+sc = plt.scatter(x, y, c=z, s=40)
+plt.colorbar(sc, label="elevation (m)")
+plt.title("Mountain Elevation")
+plt.xlabel("x coordinate (m)")
+plt.ylabel("y coordinate (m)")
+plt.show()
