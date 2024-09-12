31d30d78bd52b90da02016aa5dc6f0fe009228c0
GregSQT <gregory.souquet@hotmail.fr>
2024-09-12 22:51:59 +0200

    opti

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -2,25 +2,20 @@
 """
 Exercice 10 : Adam Upgraded
 """
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
-    """
-    Function that implements Adam gradient descent in TensorFlow.
+    '''
+    Method that creates training op for NN
+    in tf using Adam optimization algo
+    '''
 
-    Args:
-        loss: The loss of the network.
-        alpha: The learning rate, controls the step size during optimization
-        beta1: The weight used for the first moment estimation in Adam.
-        beta2: The weight used for the second moment estimation in Adam.
-        epsilon: A small number to avoid division by zero.
+    optimizer = tf.train.AdamOptimizer(learning_rate=alpha,
+                                       beta1=beta1,
+                                       beta2=beta2,
+                                       epsilon=epsilon)
 
-    Returns:
-        The Adam optimization operation.
-    """
-    return tf.train.AdamOptimizer(
-        learning_rate=alpha, beta1=beta1, beta2=beta2, epsilon=epsilon,
-        use_locking=False, name='Adam'
-    ).minimize(loss)
-    
+    train_op = optimizer.minimize(loss)
+
+    return train_op
