502d0aae3b0736863db01a9fc2e15eec03f14670
GregSQT <gregory.souquet@hotmail.fr>
2024-09-12 23:02:33 +0200

    opti

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -5,12 +5,12 @@ Exercice 10 : Adam Upgraded
 import tensorflow as tf
 
 
-def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
-    '''
-    Method that creates training op for NN
-    in tf using Adam optimization algo
-    '''
-
-    opt = tf.train.AdamOptimizer(learning_rate=alpha, beta1=beta1,
-                                 beta2=beta2, epsilon=epsilon)
-    return opt.minimize(loss)
+def create_Adam_op(alpha, beta1, beta2, epsilon):
+    """
+    Sets up the Adam optimization algorithm in TensorFlow.
+    """
+    optimizer = tf.keras.optimizers.Adam(learning_rate=alpha,
+                                         beta_1=beta1,
+                                         beta_2=beta2,
+                                         epsilon=epsilon)
+    return optimizer
