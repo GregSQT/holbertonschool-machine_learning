26e29cb898fdbbbde7806f79d0c15a08f7c365dd
GregSQT <gregory.souquet@hotmail.fr>
2024-09-12 22:46:43 +0200

    opti

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """
-Exercice 3 : Trains a loaded neural network model using mini-batch gradient descent
+Exercice 3 : Trains a loaded neural network model 
+             using mini-batch gradient descent
 """
 import numpy as np
 
 
 shuffle_data = __import__('2-shuffle_data').shuffle_data
+
 
 def create_mini_batches(X, Y, batch_size):
     '''
