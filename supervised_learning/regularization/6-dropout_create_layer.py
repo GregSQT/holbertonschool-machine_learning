820a1b8f1d5cc9dac286792b011205fcda50dd3e
GregSQT <gregory.souquet@hotmail.fr>
2024-09-20 00:36:01 +0200

    regul

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -5,24 +5,18 @@ Exercice 6 : Create a Layer with L2 Dropout
 import tensorflow as tf
 
 
-def dropout_create_layer(prev, n, activation, keep_prob):
-    """function that creates a tf layer using dropout"""
+def dropout_create_layer(prev, n, activation, keep_prob, training=True):
+    """
+    Creates a layer of a neural network using dropout.
+    """
+    # Weight initialization: He et. al
+    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode='fan_avg')
 
-    # Initialize the weights of the layer using variance_scaling_initializer
-    initializer = tf.contrib.layers.variance_scaling_initializer(
-        mode="FAN_AVG")
+    # Create Dense layer
+    dense = tf.keras.layers.Dense(units=n, activation=activation,
+                                  kernel_initializer=init)
 
-    # Create a dense layer with the specified number of neurons
-    # activation function, initializer, and name
-    layer = tf.layers.Dense(
-        units=n,
-        activation=activation,
-        kernel_initializer=initializer,
-        name='layer'
-    )
+    # Apply dropout
+    dropout = tf.nn.dropout(dense(prev), rate=1-keep_prob)
 
-    # Create a dropout layer with the specified keep probability
-    drop = tf.layers.Dropout(rate=(1 - keep_prob))
-
-    # Apply the dropout layer to the output of the dense layer
-    return drop(layer(prev))
+    return dropout
