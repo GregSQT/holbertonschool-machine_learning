8bf9f5706f9b23f379d88386cc8b8f7bdd23f1da
GregSQT <gregory.souquet@hotmail.fr>
2024-09-20 00:34:55 +0200

    regul

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -6,15 +6,15 @@ import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """function that creates a tf layer using L2 regularization"""
-    initializer = tf.contrib.layers.variance_scaling_initializer(
-        mode="FAN_AVG")
-    regularizer = tf.contrib.layers.l2_regularizer(lambtha)
-    layer = tf.layers.Dense(
+    """Creates a dense layer with L2 regularization."""
+    init_weights = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                         mode="fan_avg")
+    l2_regularizer = tf.keras.regularizers.L2(lambtha)
+    layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
-        kernel_initializer=initializer,
-        kernel_regularizer=regularizer,
-        name='layer'
+        kernel_initializer=init_weights,
+        kernel_regularizer=l2_regularizer
     )
+
     return layer(prev)
