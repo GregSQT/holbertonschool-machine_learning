542993df5d2a32a044926791646c3d6e2316030f
GregSQT <gregory.souquet@hotmail.fr>
2024-09-28 01:24:06 +0200

    c

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -2,77 +2,62 @@
 """
 Exercice 5 : LeNet-5 in Keras
 """
-# Description of the purpose of the code
-# Importing the tensorflow.keras module for deep learning
 import tensorflow.keras as K
 
 
 def lenet5(X):
-    """function that builds a modified version of LeNet-5"""
-    # Function definition with input parameter and docstring
+    """
+        function that builds a modified version of the LeNet-5
+        network architecture using keras
 
-    # Initializer for the layer weights
-    initializer = K.initializers.he_normal()
-    layer = K.layers.Conv2D(filters=6,
-                            kernel_size=5,
-                            padding='same',
-                            kernel_initializer=initializer,
-                            # Convolutional layer with 6 filters,
-                            # kernel size 5x5, same padding, ReLU activation
-                            activation='relu')
+        :param X: K.input, shape(m,28,28,1) input images
 
-    output = layer(X)  # Apply the convolutional layer to the input
-    layer = K.layers.MaxPool2D(pool_size=2,
-                               strides=2)  # Max pooling layer
-    # with pool size 2x2 and stride 2
-    output = layer(output)  # Apply the max pooling layer to the output
-    layer = K.layers.Conv2D(filters=16,
-                            kernel_size=5,
-                            padding='valid',
-                            kernel_initializer=initializer,
-                            # Convolutional layer with 16 filters,
-                            # kernel size 5x5, valid padding, ReLU activation
-                            activation='relu')
-    output = layer(output)  # Apply the convolutional layer to the output
-    layer = K.layers.MaxPool2D(pool_size=2,
-                               # Max pooling layer with
-                               # pool size 2x2 and stride 2
-                               strides=2)
-    output = layer(output)  # Apply the max pooling layer to the output
-    layer = K.layers.Flatten()  # Flatten the output to a 1D tensor
-    output = layer(output)  # Apply the flatten layer to the output
-    layer = K.layers.Dense(units=120,
-                           activation='relu',
-                           # Fully connected layer with 120 units,
-                           # ReLU activation
-                           kernel_initializer=initializer)
-    output = layer(output)  # Apply the fully connected layer to the output
-    layer = K.layers.Dense(units=84,
-                           activation='relu',
-                           # Fully connected layer with 84 units,
-                           # ReLU activation
-                           kernel_initializer=initializer)
-    output = layer(output)  # Apply the fully connected layer to the output
-    # Here, pass 'softmax' activation to the
-    # model prior to compiling/training the model
-    # Note: It is not recommended to use softmax
-    # activation as the last layer for model compilation
-    layer = K.layers.Dense(units=10,
-                           activation='softmax',
-                           # Fully connected layer with 10 units and
-                           # softmax activation
-                           kernel_initializer=initializer)
-    output = layer(output)  # Apply the fully connected layer to the output
+        Model:
+            * Convolutional layer with 6 kernels of shape 5x5 with same padding
+            * Max pooling layer with kernels of shape 2x2 with 2x2 strides
+            * Convolutional layer with 16 kernels, shape 5x5 with valid padding
+            * Max pooling layer with kernels of shape 2x2 with 2x2 strides
+            * Fully connected layer with 120 nodes
+            * Fully connected layer with 84 nodes
+            * Fully connected softmax output layer with 10 nodes
+        he_normal initialization
+        activation : relu
 
-    # Instantiate a model from the Model class, with inputs and outputs
-    model = K.models.Model(inputs=X, outputs=output)
+        :return: K.Model compiled to use Adam optmization and accuracy metrics
+    """
 
-    # Compile the model with Adam optimizer,
-    # categorical cross-entropy loss, and accuracy metric
-    # 'from_logits=True' is more numerically stable,
-    # assuming that y_pred does not encode a probability distribution
-    model.compile(optimizer=K.optimizers.Adam(),
-                  loss='categorical_crossentropy',
+    # intializer
+    initializer = K.initializers.HeNormal()
+
+    model = K.Sequential([
+        K.layers.Conv2D(filters=6,
+                        kernel_size=5,
+                        padding='same',
+                        kernel_initializer=initializer,
+                        activation='relu'),
+        K.layers.MaxPooling2D(pool_size=2,
+                              strides=2),
+        K.layers.Conv2D(filters=16,
+                        kernel_size=5,
+                        padding='valid',
+                        kernel_initializer=initializer,
+                        activation='relu'),
+        K.layers.MaxPooling2D(pool_size=2,
+                              strides=2),
+        K.layers.Flatten(),
+        K.layers.Dense(120,
+                       activation='relu',
+                       kernel_initializer=initializer),
+        K.layers.Dense(84,
+                       activation='relu',
+                       kernel_initializer=initializer),
+        K.layers.Dense(10,
+                       kernel_initializer=initializer,
+                       activation='softmax')
+    ])
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=K.optimizers.Adam(),
                   metrics=['accuracy'])
 
     return model
