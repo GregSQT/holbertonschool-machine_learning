82a2585a7a702c798149b80ab9c6f371f12fe1bc
GregSQT <gregory.souquet@hotmail.fr>
2024-09-28 03:10:46 +0200

    c

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -3,60 +3,84 @@
 Exercice 2 : Identity Block
 """
 import tensorflow.keras as K
+inception_block = __import__('0-inception_block').inception_block
 
 
-def identity_block(A_prev, filters):
+def inception_network():
     """
-    function that builds an identity block
-    as described in Deep Residual Learning for Image Recognition (2015)
+        build inception network as described in
+        'Going Deeper with convolutions'
+
+    :return: Keras model
     """
-    # Use He normal initializer for the weights
-    initializer = K.initializers.he_normal()
+    # define input
+    inputs = K.Input(shape=(224, 224, 3))
 
-    # First component of the identity block: 1x1 Convolution,
-    # BatchNormalization, ReLU Activation
-    F11_layer = K.layers.Conv2D(
-        filters=filters[0],
-        kernel_size=1,
-        padding='same',
-        kernel_initializer=initializer,
-        activation=None)
-    F11_output = F11_layer(A_prev)
-    F11_norm = K.layers.BatchNormalization()
-    F11_output = F11_norm(F11_output)
-    F11_activ = K.layers.Activation('relu')
-    F11_output = F11_activ(F11_output)
+    # convolution
+    conv1 = K.layers.Conv2D(64,
+                            kernel_size=(7, 7),
+                            activation='relu',
+                            strides=(2, 2),
+                            padding='same')(inputs)
 
-    # Second component of the identity block: 3x3 Convolution,
-    # BatchNormalization, ReLU Activation
-    F3_layer = K.layers.Conv2D(
-        filters=filters[1],
-        kernel_size=3,
-        padding='same',
-        kernel_initializer=initializer,
-        activation=None)
-    F3_output = F3_layer(F11_output)
-    F3_norm = K.layers.BatchNormalization()
-    F3_output = F3_norm(F3_output)
-    F3_activ = K.layers.Activation('relu')
-    F3_output = F3_activ(F3_output)
+    # Max pool
+    pool1 = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                  strides=(2, 2),
+                                  padding='same')(conv1)
 
-    # Third component of the identity block: 1x1 Convolution,
-    # BatchNormalization
-    F12_layer = K.layers.Conv2D(
-        filters=filters[2],
-        kernel_size=1,
-        padding='same',
-        kernel_initializer=initializer,
-        activation=None)
-    F12_output = F12_layer(F3_output)
-    F12_norm = K.layers.BatchNormalization()
-    F12_output = F12_norm(F12_output)
+    # convolution
+    conv2 = K.layers.Conv2D(64,
+                            kernel_size=(1, 1),
+                            activation='relu',
+                            strides=(1, 1),
+                            padding='same')(pool1)
+    conv3 = K.layers.Conv2D(192,
+                            kernel_size=(3, 3),
+                            activation='relu',
+                            strides=(1, 1),
+                            padding='same')(conv2)
 
-    # Add the input (residual connection) and output together
-    output = K.layers.Add()([F12_output, A_prev])
+    # Max pool
+    pool2 = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                  strides=(2, 2),
+                                  padding='same')(conv3)
 
-    # Activate the combined output with ReLU
-    output = K.layers.Activation('relu')(output)
+    # Inception 3a, 3b
+    inception3a = inception_block(pool2, [64, 96, 128, 16, 32, 32])
+    inception3b = inception_block(inception3a, [128, 128, 192, 32, 96, 64])
 
-    return output
+    # Max Pool
+    pool3 = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                  strides=(2, 2),
+                                  padding='same')(inception3b)
+
+    # Inception 4a, 4b, 4c, 4d, 4e
+    inception4a = inception_block(pool3, [192, 96, 208, 16, 48, 64])
+    inception4b = inception_block(inception4a, [160, 112, 224, 24, 64, 64])
+    inception4c = inception_block(inception4b, [128, 128, 256, 24, 64, 64])
+    inception4d = inception_block(inception4c, [112, 144, 288, 32, 64, 64])
+    inception4e = inception_block(inception4d, [256, 160, 320, 32, 128, 128])
+
+    # Max pool
+    pool4 = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                  strides=(2, 2),
+                                  padding='same')(inception4e)
+
+    # Inception 5a, 5b
+    inception5a = inception_block(pool4, [256, 160, 320, 32, 128, 128])
+    inception5b = inception_block(inception5a, [384, 192, 384, 48, 128, 128])
+
+    # Avg pool
+    pool5 = K.layers.AveragePooling2D(pool_size=(7, 7),
+                                      strides=(1, 1))(inception5b)
+
+    # dropout
+    drop1 = K.layers.Dropout(rate=0.4)(pool5)
+
+    # linear
+    output = K.layers.Dense(units=1000,
+                            activation='softmax')(drop1)
+
+    model = K.models.Model(inputs, output)
+
+    return model
