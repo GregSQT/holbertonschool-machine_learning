069989a2377a6fbc3150ff3f262bfe9f32f262d7
GregSQT <gregory.souquet@hotmail.fr>
2024-09-28 04:10:08 +0200

    c

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -2,64 +2,56 @@
 """
 Dense Block
 """
-import tensorflow.keras as K
+from tensorflow import keras as K
 
 
 def dense_block(X, nb_filters, growth_rate, layers):
     """
-    Function that builds a dense block as described
-    in Densely Connected Convolutional Networks
+    Builds a dense block as described in
+    'Densely Connected Convolutional Networks' (2016)'
+
+    Parameters:
+    X : tensor
+        The output of the previous layer.
+    nb_filters : int
+        The number of filters in X.
+    growth_rate : int
+        The growth rate for the dense block.
+    layers : int
+        The number of layers in the dense block.
+
+    Returns:
+    tensor, int
+        The concatenated output of each layer within the Dense Block
+        and the number of filters within the concatenated outputs,
+        respectively.
     """
+    init = K.initializers.HeNormal(seed=0)
+    concat_features = X
 
-    # Use the He normal initializer for the weights
-    initializer = K.initializers.he_normal()
+    for _ in range(layers):
+        # Batch Normalization and ReLU
+        bn1 = K.layers.BatchNormalization(axis=-1)(concat_features)
+        relu1 = K.layers.Activation('relu')(bn1)
 
-    # Loop over the specified number of layers
-    for i in range(layers):
+        # 1x1 Convolution (Bottleneck layer)
+        conv1 = K.layers.Conv2D(4 * growth_rate,
+                                (1, 1),
+                                padding='same',
+                                kernel_initializer=init)(relu1)
 
-        # Batch normalization and ReLU activation for X (input)
-        l1_norm = K.layers.BatchNormalization()
-        l1_output = l1_norm(X)
-        l1_activ = K.layers.Activation('relu')
-        l1_output = l1_activ(l1_output)
+        # Batch Normalization and ReLU
+        bn2 = K.layers.BatchNormalization(axis=-1)(conv1)
+        relu2 = K.layers.Activation('relu')(bn2)
 
-        # 1x1 Convolution with 4 times the growth rate
-        # filters and no activation function applied yet
-        l1_layer = K.layers.Conv2D(filters=4 * growth_rate,
-                                   kernel_size=1,
-                                   padding='same',
-                                   kernel_initializer=initializer,
-                                   activation=None)
-        l1_output = l1_layer(l1_output)
+        # 3x3 Convolution
+        conv2 = K.layers.Conv2D(growth_rate,
+                                (3, 3), padding='same',
+                                kernel_initializer=init)(relu2)
 
-        # Batch normalization and ReLU activation for
-        # the output of the first convolution
-        l2_norm = K.layers.BatchNormalization()
-        l2_output = l2_norm(l1_output)
-        l2_activ = K.layers.Activation('relu')
-        l2_output = l2_activ(l2_output)
+        # Concatenate input with output of the 3x3 convolution
+        concat_features = K.layers.Concatenate(axis=-1)([concat_features,
+                                                         conv2])
+        nb_filters += growth_rate
 
-        # 3x3 Convolution with the growth rate number of filters
-        # and no activation function applied yet
-        l2_layer = K.layers.Conv2D(filters=growth_rate,
-                                   kernel_size=3,
-                                   padding='same',
-                                   kernel_initializer=initializer,
-                                   activation=None)
-        l2_output = l2_layer(l2_output)
-
-        # Concatenate the outputs of the branches (input & output) channel-wise
-        X = K.layers.concatenate([X, l2_output])
-
-        # (Optionally) infer the number of channels
-        # in the output (after concatenation)
-        # nb_filters += growth_rate
-
-    # (Optionally) activate the combined output with ReLU
-    # output = K.layers.Activation('relu')(output)
-
-    # Return the concatenated output and the updated number of filters
-    return X, X.shape[-1]
-    # Alternatively, this return works as well:
-    # return X, nb_filters
-    
+    return concat_features, nb_filters
