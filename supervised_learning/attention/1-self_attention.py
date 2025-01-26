7bc08df7a87a0023d475764969b9c45f06fa277c
GregSQT <9604@holbertonstudents.com>
2025-01-26 08:27:37 +0100

    a

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -33,7 +33,8 @@ class SelfAttention(tf.keras.layers.Layer):
         containing the outputs of the encoder
         Returns: context, weights"""
         s_prev_expanded = tf.expand_dims(s_prev, 1)
-        score = self.V(tf.nn.tanh(self.W(s_prev_expanded) + self.U(hidden_states)))
+        score = self.V(tf.nn.tanh(self.W(s_prev_expanded) + 
+                                  self.U(hidden_states)))
         weights = tf.nn.softmax(score, axis=1)
         context = tf.reduce_sum(weights * hidden_states, axis=1)
         return context, weights
