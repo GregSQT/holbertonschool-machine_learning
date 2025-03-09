190bc46c643f29b6b9a3bd01ee8c80c41e300620
GregSQT <9604@holbertonstudents.com>
2025-03-10 00:14:04 +0100

    sdf

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""
+the function def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1, 
+gamma=0.99, epsilon=1, min_epsilon=0.1, epsilon_decay=0.05): that performs SARSA(λ)
+"""
 import gym
 import numpy as np
 
